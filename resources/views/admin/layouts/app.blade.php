2760d62fc3e806565e935d5e658db7a6fa63ec57
Sreydeth1819 <sreydeth.khon@student.passerellesnumeriques.org>
2026-07-07 20:03:42 +0700

    fix: dashbord side bar

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -82,14 +82,20 @@
                         ],
                     ],
                     'news' => [
-                        'label' => 'News & Resources',
+                        'label' => 'News',
                         'icon' => 'M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z',
                         'children' => [
                             ['route' => 'admin.news.index', 'label' => 'News Articles'],
                             ['route' => 'admin.categories.index', 'label' => 'Categories'],
+                        ],
+                    ],
+                    'resources' => [
+                        'label' => 'Resources',
+                        'icon' => 'M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z',
+                        'children' => [
                             ['route' => 'admin.reports.index', 'label' => 'Annual Reports'],
-                            ['route' => 'admin.media.index', 'label' => 'Media Gallery'],
-                            ['route' => 'admin.downloads.index', 'label' => 'Downloads'],
+                            ['route' => 'admin.media.index', 'label' => 'Media'],
+                            ['route' => 'admin.wap.index', 'label' => 'Words and Pictures Application'],
                         ],
                     ],
                     'involved' => [
