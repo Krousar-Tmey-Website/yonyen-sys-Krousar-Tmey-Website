4f13bd5da5d941fba7e6485ccae2e73a975fe1aa
sokyangLakk <sokyang.lakk@student.passerellesnumeriques.org>
2026-07-17 15:45:30 +0700

    commit feature

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -390,7 +390,7 @@ function googleTranslateElementInit() {
                             <img src="https://flagcdn.com/w20/kh.png" class="w-4 h-auto rounded-sm" alt="Khmer"> KM
                         </button>
                     </div>
-                    </div>
+                </div>
                     <a href="{{ route('donate') }}" class="btn-primary w-full justify-center py-3">
                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
