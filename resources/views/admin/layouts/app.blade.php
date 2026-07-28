0d4dd434fdc16e71c5866f264c4d5c16909f2641
Kimdarika <kimdarika2006@gmail.com>
2026-07-28 11:23:47 +0700

    feat: banner page of contect

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -161,6 +161,7 @@
                 'icon' =>
                 'M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z',
                 'children' => [
+                ['route' => 'admin.contact-page.index', 'label' => __('Contact Page')],
                 ['route' => 'admin.contacts.index', 'label' => __('Contact Messages')],
                 ['route' => 'admin.offices.index', 'label' => __('Offices')],
                 ['route' => 'admin.newsletter.index', 'label' => __('Subscribers')],
